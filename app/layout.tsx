10fd9bc1945c1392a773d1a3f9631c4f0722e1ce
araweri <araweri@advancedtax.com.au>
2026-07-08 14:52:04 +1000

    Update Google Tag Manager ID in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,14 +42,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WJ2LLTVM');`
+})(window,document,'script','dataLayer','GTM-MNZ3BWSF');`
           }}
         />
       </head>
       <body>
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-WJ2LLTVM"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MNZ3BWSF"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
